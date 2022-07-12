031b6f292da751e05a2d8d95173697791f37d4cb
Alex Reeser <areeser@corebridge.net>
2022-07-11 22:05:41 -0500

    SFX base

---
+++ attochatter/SFX/SoundEffects.cs
@@ -0,0 +1,19 @@
+﻿using System.Media;
+
+namespace attochatter
+{
+    public enum SoundEffect
+    {
+        bubble_pop,
+        doorbell
+    }
+    public class SoundEffects
+    {
+        public void Play(SoundEffect effect)
+        {
+            string file = effect == SoundEffect.doorbell ? "doorbell" : "bubble_pop";
+            SoundPlayer player = new SoundPlayer("./SFX/"+file+".wav");
+            player.Play();
+        }
+    }
+}
